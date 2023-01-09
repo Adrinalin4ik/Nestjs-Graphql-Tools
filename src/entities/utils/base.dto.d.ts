2b9efc9693d19651c669eb62c4bbe0de9fe04aa5
Alexey Panfilkin <soild@mail.ru>
2023-01-09 21:11:57 +0300

    remove useless files

--- src/entities/utils/base.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class BaseDTO {
-    id: number;
-    created_at: Date;
-    updated_at: Date;
-}
--- src/entities/utils/base.entity.d.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEntity } from 'typeorm';
-export declare class Base extends BaseEntity {
-    id: number;
-    created_at: Date;
-    updated_at: Date;
-}
