4ff79ba80eb3987129c832086897b9442a157a40
Alexey Panfilkin <soild@mail.ru>
2023-11-07 11:30:54 +0300

    Bump deps

--- src/entities/story/story.resolver.ts
+++ src/entities/story/story.resolver.ts
@@ -1,6 +1,21 @@
 // import { Filter } from '@nestjs-query/core';
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver } from '@nestjs/graphql';
 import { StoryModel } from './story.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Filter, FilterArgs } from '../../../lib';
 
 @Resolver(() => StoryModel)
-export class StoryResolver {}
+export class StoryResolver {
+
+  constructor(@InjectRepository(StoryModel) public readonly storyRepository: Repository<StoryModel>,) {
+
+  }
+
+  @Query(() =>[StoryModel])
+  async stories(
+    @Filter(() => StoryModel) filter: FilterArgs,
+  ) {
+    return this.storyRepository.createQueryBuilder().where(filter).getMany();
+  }
+}
